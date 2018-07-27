22ad429465ffef9f0399fd68bed36d606ab132d8
Demo User <demouser@MacBook-Pro.local>
2018-07-27 09:39:12 -0400

    added reload counter

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import jinja2
 import webapp2
 from models import User
+from models import Visitor
 
 jinja_current_directory = jinja2.Environment(loader=jinja2.FileSystemLoader(
 os.path.dirname(__file__)), extensions=['jinja2.ext.autoescape'], autoescape=True)
@@ -14,6 +15,12 @@ class WelcomePage(webapp2.RequestHandler):
 
 class GamePage(webapp2.RequestHandler):
     def get(self):
+        if Visitor.query(Visitor.plays > 0).get():
+            newVisitor = Visitor.query(Visitor.plays >= 0).get()
+            newVisitor.plays += 1
+            newVisitor.put()
+        else:
+            Visitor(plays = 1).put()
         self.response.headers['Content-Type'] = 'text/html'
         template_vars = {"username": self.request.get("username")}
         template = jinja_current_directory.get_template('flappypotato.html')
--- models.py
+++ models.py
@@ -3,3 +3,6 @@ from google.appengine.ext import ndb
 class User(ndb.Model):
     username = ndb.StringProperty()
     score = ndb.IntegerProperty()
+
+class Visitor(ndb.Model):
+    plays = ndb.IntegerProperty()
